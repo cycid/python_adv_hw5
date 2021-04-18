98cc99e0c0a1f45b75c607ccb3eb8be3c62d80e2
cycic <cycid90@gmail.com>
2021-04-18 19:43:12 +0300

    done

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+from cycidLibrary import main
+setup(
+    name="cycidLibrary",
+    version="0.0.2",
+    author="none",
+    desctiption="my own library project",
+    url="https://github.com/cycid/python_adv_hw5",
+    packages=find_packages()
+)
+
